b4c274b6680709c34cde41cb680a23c32122e929
Sontara444 <sontararajput@gmail.com>
2023-08-08 23:17:48 +0530

    practice

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const chatInput = document.querySelector(".chat-input textarea")
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage;
+const createChatLi = (message, className) =>{
+    const chatLi = document.createElement("li");
+
+}
 
 const handleChat = ()=>{
     userMessage = chatInput.value.trim();
     console.log(userMessage);
+    if(!userMessage) return;
+
+    createChatLi(userMessage, "outgoing")
 }
  
 sendChatBtn.addEventListener("click", handleChat );
